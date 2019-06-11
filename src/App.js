615a0e2b5f29d85b618865055601ae147e131e67
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-11 17:55:45 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ class App extends React.Component {
   render() {
     return (
         <div>
+          {
           this.state.space.map(p => <h1 key={p.id} p={p}>{p.name}</h1>)
+          }
         </div>
       );
   }

87921f1d959417a93598fd6699f7ad441eb986d1
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-11 14:07:47 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React from 'react'
+
+class App extends React.Component {
+
+  state = {
+    space: []
+  }
+
+  componentDidMount(){
+    fetch("http://api.open-notify.org/astros.json")
+      .then(res => res.json())
+      .then(({people}) => this.setState({space: people}))
+  }
+
+  //fetch("http://api.open-notify.org/astros.json")
+      // .then(res => res.json())
+      // .then(data => console.log(data.people))
+
+  render() {
+    return (
+        <div>
+          {
+            this.state.space.map(p => <h1 key={p.id} p={p}>{p.name}</h1>)
+          }
+        </div>
+      );
+  }
+}
+
+export default App

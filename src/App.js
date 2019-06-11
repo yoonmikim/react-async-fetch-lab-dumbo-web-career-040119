e45da65137ad61a86706e2780ff98549408ae416
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-11 17:50:23 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import React from 'react'
 class App extends React.Component {
   
   componentDidMount(){
-    fetch()
+    fetch("http://api.open-notify.org/astros.json")
+      .then(res => res.json())
+      .then(console.log)
   }
   
   render() {
